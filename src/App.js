a98fa76832bd980f7dad8679a8f6fcde350e5060
JooYoo <jooyoo@outlook.com>
2021-07-26 14:35:44 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Pomodoro from "./components/Pomodoro";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Pomodoro />
+    </div>
+  );
+}
---
+++ src/components/Pomodoro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pomodoro = () => {
+  return (
+    <>
+      <h4>Pomodoro</h4>
+    </>
+  );
+};
+
+export default Pomodoro;
