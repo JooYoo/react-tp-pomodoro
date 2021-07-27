f9b35afcb5c3b76503bdd5995aab8e12498b63a9
JooYoo <jooyoo@outlook.com>
2021-07-27 14:54:27 +0200

    feat: counter

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Pomodoro = () => {
+  const [restTime, setRestTime] = useState(0);
+
+  const increaseTimeHandler = () => {
+    setRestTime((prev) => {
+      return prev + 1;
+    });
+  };
+
+  const decreaseTimeHandler = () => {
+    setRestTime((prev) => {
+      return prev - 1;
+    });
+  };
+
+  const resetTimeHandler = () => {
+    setRestTime(0);
+  };
+
   return (
     <>
       <h4>Pomodoro</h4>
+      <h5>{restTime}</h5>
+      <button onClick={decreaseTimeHandler}>-</button>
+      <button onClick={resetTimeHandler}>0</button>
+      <button onClick={increaseTimeHandler}>+</button>
+      <hr />
+      <button>Start</button>
+      <button>Pause</button>
+      <button>Stop</button>
     </>
   );
 };
