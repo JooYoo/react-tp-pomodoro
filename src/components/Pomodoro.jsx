be5c23cf3ca15b652e4f30d3b677bf0a40e447f8
JooYoo <jooyoo@outlook.com>
2021-08-03 11:28:52 +0200

    feat: sec min both works

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,48 +1,72 @@
 import React, { useEffect, useState } from "react";
 
 const Pomodoro = () => {
-  const [min, setMin] = useState(0);
-  const [sec, setSec] = useState("00");
+  const initMin = 0;
+  const initSec = "00";
+  const [min, setMin] = useState(initMin);
+  const [sec, setSec] = useState(initSec);
 
   // watch min
   useEffect(() => {
-    if (sec === 0) {
-      clearAllInterval();
+    // count down finished
+    if (min === 0) {
+      stopHandler();
 
-      // TODO: decrease Min
+      // TODO: alarm CountDown finish
     }
-  }, [sec]);
+
+    // the sec which updates the Min
+    // TODO: sec === 0
+    if (sec === 55) {
+      // 59 ~ 0 repeat
+      clearAllInterval();
+      countDownSec();
+
+      // decrease Min
+      decreaseMinHandler();
+    }
+  }, [min, sec]);
 
   // counter control
-  const increaseTimeHandler = () => {
+  const increaseMinHandler = () => {
     setMin((prev) => {
       return prev + 1;
     });
   };
 
-  const decreaseTimeHandler = () => {
+  const decreaseMinHandler = () => {
     setMin((prev) => {
       return prev - 1;
     });
   };
 
-  const resetTimeHandler = () => {
-    setMin(0);
+  const resetMinHandler = () => {
+    setMin(initMin);
   };
 
   // timer control
   const clearAllInterval = () => {
-    for (let i = 0; i < 100; i++) {
+    // Get a reference to the last interval + 1
+    const interval_id = window.setInterval(function () {},
+    Number.MAX_SAFE_INTEGER);
+
+    // Clear any timeout/interval up to that id
+    for (let i = 1; i < interval_id; i++) {
       window.clearInterval(i);
     }
   };
 
   const startHandler = () => {
+    // TODO: condition for disable start button
+
     // decrase Sec
     countDownSec();
   };
 
   const countDownSec = () => {
+    // set init Min
+    setSec(initSec);
+
     // spend 1s: "00" -> 59
     setTimeout(() => {
       setSec(60);
@@ -57,7 +81,12 @@ const Pomodoro = () => {
   };
 
   const stopHandler = () => {
+    // clear Interval
     clearAllInterval();
+
+    // reset min and sec
+    setMin(initMin);
+    setSec(initSec);
   };
 
   return (
@@ -66,9 +95,9 @@ const Pomodoro = () => {
       <h5>
         {min}:{sec}
       </h5>
-      <button onClick={decreaseTimeHandler}>-</button>
-      <button onClick={resetTimeHandler}>0</button>
-      <button onClick={increaseTimeHandler}>+</button>
+      <button onClick={decreaseMinHandler}>-</button>
+      <button onClick={resetMinHandler}>0</button>
+      <button onClick={increaseMinHandler}>+</button>
       <hr />
       <button onClick={startHandler}>Start</button>
       <button onClick={stopHandler}>Stop</button>
